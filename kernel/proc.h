a642efa9808f21fb3dc037a23eb1b4e2db375c6c
hazemgamaal <hazem.gme@gmail.com>
2026-05-15 20:15:01 +0300

    feat: add freeze_reason[32] to PCB

--- kernel/proc.h
+++ kernel/proc.h
@@ -91,7 +91,9 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
-
+  int frozen;               // 1 if process is frozen
+  char freeze_reason[32];   // human-readable freeze reason
+  
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
 
